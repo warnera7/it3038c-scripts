e396bef31a413186748b50e49c9adbdc64d4cebd
warnera7 <warnera7@mail.uc.edu>
2022-10-07 17:55:53 -0400

    lab 6 completed

--- node/server.js
+++ node/server.js
@@ -14,6 +14,21 @@ http.createServer((req, res) => {
     
     } else if(req.url.match("/sysinfo")) {
         myHostName=os.hostname();
+        serverUptime=os.uptime();
+        //this was super fun to get working correctly, based off the comment by Christian Meyer at https://stackoverflow.com/questions/13903897/javascript-return-number-of-days-hours-minutes-seconds-between-two-dates
+        dayUptime = (serverUptime / 86400);
+        hourUptime = (dayUptime % 1) * (24);
+        minuteUptime = (hourUptime % 1) * (60);
+        secondsUptime = (minuteUptime % 1) * (60);
+        //testUptime = [Math.floor(serverUptime), Math.floor(dayUptime), Math.floor(hourUptime), Math.floor(minuteUptime), Math.floor(secondsUptime)];
+
+        totalMem=os.totalmem();
+        totalMBMem = (totalMem / 1000000).toFixed(2);
+        freeMem=os.freemem();
+        freeMBMem = (freeMem / 1000000).toFixed(2);
+
+        CPUdata=os.cpus().length;
+        
         html=`
         <!DOCTYPE html>
         <html>
@@ -23,14 +38,15 @@ http.createServer((req, res) => {
           <body>
             <p>Hostname: ${myHostName}</p>
             <p>IP: ${ip.address()}</p>
-            <p>Server Uptime: </p>
-            <p>Total Memory: </p>
-            <p>Free Memory: </p>
-            <p>Number of CPUs: </p>
+            <p>Server Uptime: Days: ${Math.floor(dayUptime)}, Hours: ${Math.floor(hourUptime)}, Minutes: ${Math.floor(minuteUptime)}, Seconds: ${secondsUptime.toFixed(0)}</p>
+            <p>Total Memory: ${totalMBMem} MB</p>
+            <p>Free Memory: ${freeMBMem} MB</p>
+            <p>Number of CPUs: ${CPUdata}</p>
           </body>
         </html>`
         res.writeHead(200, {"Content-Type": "text/html"});
         res.end(html); 
+    
     }  else {
         res.writeHead(404, {"Content-Type": "text/plain"});
         res.end(`404 File not found at ${req.url}`);
