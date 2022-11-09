54b61c68b8efd56583d5bcef57e9471faafb364b
warnera7 <warnera7@mail.uc.edu>
2022-11-09 15:17:41 -0500

    forgot something

--- labs/lab_9/widgetsApi.py
+++ labs/lab_9/widgetsApi.py
@@ -9,10 +9,10 @@ encoData = r
 decodeData = encoData.decode("utf-8")
 
 #printing the widegets and their assoicated colors in a janky way
-print("W%s is %s" % (decodeData[11:17],decodeData[28:32]))
-print("W%s is %s" % (decodeData[45:51],decodeData[62:67]))
-print("W%s is %s" % (decodeData[80:86],decodeData[97:102]))
-print("W%s is %s" % (decodeData[115:121],decodeData[132:136]))
+print("W%s is %s." % (decodeData[11:17],decodeData[28:32]))
+print("W%s is %s." % (decodeData[45:51],decodeData[62:67]))
+print("W%s is %s." % (decodeData[80:86],decodeData[97:102]))
+print("W%s is %s." % (decodeData[115:121],decodeData[132:136]))
 
 #[10:17] - widget 1, [28:32] - widget1 color
 #[44:51] - widget 2, [62:67] - widget2 color
